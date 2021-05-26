8e15a7bb85a21146fe86fd64248e5968f5a4161b
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2021-05-26 16:42:25 +0900

    Add mysql2 connection

--- src/mysql/mysql_processor.ts
+++ src/mysql/mysql_processor.ts
@@ -1,17 +1,32 @@
+import mysql2 from "mysql2"
 import { Statement } from "../models"
 import { DdlSyncProcessor } from "../processor"
 import { MySqlParser } from "./mysql_parser"
 
 export default class MysqlProcessor extends DdlSyncProcessor {
+  private con
+
   constructor(
     config: { [key: string]: any },
     dryrun: boolean,
   ) {
     super(config, dryrun)
+    this.con = mysql2.createConnection({
+      host: config.host,
+      port: config.port,
+      user: config.user,
+      password: config.password,
+      database: config.database,
+    })
   }
 
   protected async init() {
-    return {}
+    const options = {} as any
+    const result = await this.con.promise().query("SELECT version() AS version") as [any[], any[]]
+    if (result[0].length) {
+      options.version = result[0][0].version
+    }
+    return options
   }
 
   protected async parse(input: string, options: { [key: string]: any }) {
@@ -23,5 +38,6 @@ export default class MysqlProcessor extends DdlSyncProcessor {
   }
 
   async destroy() {
+    this.con.destroy()
   }
 }
